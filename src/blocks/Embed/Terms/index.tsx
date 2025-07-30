cae301c9f67d6b5944b5674ec100d5ea6ba38524
James Bryan <james.bryan.home@gmail.com>
2025-07-30 07:54:59 -0500

    feat: refactor Privacy and Terms components to use Policy component for loading policies

--- src/blocks/Embed/Terms/index.tsx
+++ src/blocks/Embed/Terms/index.tsx
@@ -1,21 +1,7 @@
+'use client';
+
+import Policy from '@/blocks/Embed/policy';
+
 export const Terms: React.FC = () => {
-  return (
-    <div>
-      <div id="TDNwaWIwaG5jV0ZFT0ZkWE9WRTlQUT09" className="policy_embed_div">
-        Please wait while the policy is loaded. If it does not load, please{' '}
-        <a
-          rel="nofollow"
-          href="https://policies.termageddon.com/api/policy/TDNwaWIwaG5jV0ZFT0ZkWE9WRTlQUT09"
-          target="_blank"
-        >
-          click here
-        </a>{' '}
-        to view the policy.
-      </div>
-      <script
-        src="https://policies.termageddon.com/api/embed/TDNwaWIwaG5jV0ZFT0ZkWE9WRTlQUT09.js"
-        defer
-      />
-    </div>
-  );
+  return <Policy policyKey="TDNwaWIwaG5jV0ZFT0ZkWE9WRTlQUT09" />;
 };
---
+++ src/blocks/Embed/policy.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+
+type Props = {
+  policyKey?: string;
+};
+
+const termageddonAPIPath = 'https://policies.termageddon.com/api/policy/';
+
+const Policy = ({ policyKey }: Props) => {
+  useEffect(() => {
+    const policy = document.getElementById('policy');
+    if (policy === null || policyKey === undefined) {
+      console.log('Error! Could not find policy element or policy key.');
+    } else {
+      const pol_key = policyKey;
+      const pol_extra = policy.dataset.extra ? '?' + policy.dataset.extra : '';
+      const xhr = new XMLHttpRequest();
+      xhr.onload = () => {
+        console.log('Policy loaded!');
+        policy.innerHTML = xhr.responseText;
+      };
+
+      xhr.onerror = function () {
+        console.log('Error! Could not load policy.');
+        policy.innerHTML = 'There has been an error loading this policy!';
+      };
+
+      xhr.open('GET', termageddonAPIPath + pol_key + pol_extra);
+      xhr.send();
+    }
+  });
+
+  return (
+    <div
+      id="policy"
+      data-extra="h-align=left&h-depth=3&table-style=accordion"
+    />
+  );
+};
+
+export default Policy;
