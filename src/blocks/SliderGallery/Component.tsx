700681b2280ff88820beba6f6c441e089ec65749
James Bryan <james.bryan.home@gmail.com>
2025-09-15 16:45:26 -0500

    feat: mobile improvements to SliderGalleryBlock

--- src/blocks/SliderGallery/Component.tsx
+++ src/blocks/SliderGallery/Component.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import { ChevronRight } from 'lucide-react';
+import Balancer from 'react-wrap-balancer';
 
 import { LinkedTextBlock } from '@/blocks/LinkedText/Component';
 import { CMSLink } from '@/components/Link';
@@ -45,7 +46,7 @@ export const SliderGalleryBlock: React.FC<SliderGalleryProps> = ({
       {showMegaTitle && megaTitle && (
         <h2
           className={cn(
-            'relative -z-10 -mb-12 sm:-mb-20 md:-mb-80 flex justify-center overflow-hidden text-center text-[4rem] sm:text-[6rem] md:text-[20rem] lg:text-[27rem] leading-[4rem] sm:leading-[6rem] md:leading-[28rem] lg:leading-[38rem] whitespace-nowrap opacity-30',
+            'relative -z-10 -mb-12 hidden justify-center overflow-hidden text-center text-[4rem] leading-[4rem] whitespace-nowrap opacity-10 mix-blend-multiply sm:-mb-40 sm:flex sm:text-[13rem] sm:leading-[18rem] md:-mb-60 md:text-[20rem] md:leading-[28rem] xl:-mb-80 xl:text-[27rem] xl:leading-[38rem]',
 
             megaTitleClass[titleColor || 'none'],
           )}
@@ -55,13 +56,13 @@ export const SliderGalleryBlock: React.FC<SliderGalleryProps> = ({
       )}
 
       {/* Image Grid */}
-      <div className="relative grid h-full w-full grid-cols-1 lg:grid-cols-[50%_1fr] gap-4 sm:gap-6 overflow-hidden px-3 sm:px-4 md:px-0">
+      <div className="relative container grid h-full w-full grid-cols-1 gap-4 sm:gap-6 xl:max-w-full xl:grid-cols-[50%_1fr] xl:overflow-hidden">
         {/* Subtitle */}
         {showSubtitle && subtitle && (
-          <div className="absolute top-6 sm:top-10 md:top-36 left-1/2 z-10 container grid w-full -translate-x-1/2 grid-cols-1 lg:grid-cols-[50%_1fr]">
+          <div className="absolute top-40 left-1/2 z-10 container hidden w-full -translate-x-1/2 -translate-y-1/2 xl:grid xl:grid-cols-[50%_1fr]">
             <LinkedTextBlock
               blockType="linkedText"
-              className="w-full px-3 sm:px-4 lg:px-0"
+              className="w-full pl-6"
               leftText={subtitle}
               rightText=""
             />
@@ -70,27 +71,27 @@ export const SliderGalleryBlock: React.FC<SliderGalleryProps> = ({
         {/* Main Active Image */}
         <Media
           resource={images[activeImageIndex]?.image}
-          imgClassName="object-cover object-center h-[300px] sm:h-[400px] md:h-[600px] lg:h-[840px] w-full lg:max-w-[1100px] rounded-lg lg:justify-self-end opacity-100"
+          imgClassName="object-cover object-center h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px] xl:h-[900px] w-full xl:max-w-[1100px] rounded-lg xl:justify-self-end opacity-100"
         />
 
         <div className="relative flex h-full flex-col gap-4 sm:gap-6">
           {/* Thumbnail Images */}
           <div className="relative flex flex-col gap-4 sm:gap-6">
-            <div className="flex flex-row gap-3 sm:gap-4 md:gap-6 overflow-x-auto pb-3 sm:pb-4 scrollbar-hide">
+            <div className="scrollbar-hide flex flex-row gap-3 overflow-x-auto pb-3 sm:gap-4 sm:pb-4 md:gap-6">
               {images.map((_, i) => {
                 const index = (activeImageIndex + i) % images.length; // Calculate reordered index
                 if (index === activeImageIndex) return null; // Skip active image
                 return (
                   <div
                     key={index}
-                    className="relative h-20 w-20 sm:h-32 sm:w-32 md:h-64 lg:h-80 md:w-64 lg:w-80 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg saturate-0 hover:saturate-100 transition-all duration-300"
+                    className="relative h-20 w-20 flex-shrink-0 cursor-pointer overflow-hidden rounded-lg saturate-0 transition-all duration-300 hover:saturate-100 sm:h-32 sm:w-32 md:h-48 md:w-48 lg:h-72 lg:w-72 xl:h-80 xl:w-80"
                     onClick={() => handleClick(index)}
                   >
                     <Media
                       resource={images[index]?.image}
                       imgClassName="absolute h-full w-full object-cover hover:scale-110 transition-transform duration-300"
                     />
-                    <h6 className="text-convergence-beige absolute right-1 sm:right-2 md:right-8 bottom-1 sm:bottom-2 md:bottom-5 w-full text-right text-[10px] sm:text-xs md:text-base leading-tight">
+                    <h6 className="text-convergence-beige absolute right-8 bottom-5 hidden w-full text-right leading-tight lg:block">
                       {images[index]?.title}
                     </h6>
                   </div>
@@ -100,17 +101,17 @@ export const SliderGalleryBlock: React.FC<SliderGalleryProps> = ({
           </div>
 
           {/* Image Content */}
-          <div className="relative flex h-auto lg:h-2/3 max-w-full lg:max-w-[600px] flex-col pt-4 sm:pt-8 md:pt-32 px-3 sm:px-4 lg:pl-8">
-            <h2 className="text-convergence-brown text-lg sm:text-2xl md:text-4xl lg:text-5xl leading-tight">
-              {images[activeImageIndex]?.title}
+          <div className="relative flex h-auto max-w-full flex-col px-3 pt-4 sm:px-4 sm:pt-8 md:pt-16 lg:h-2/3 lg:max-w-[600px] lg:pt-32 lg:pl-8">
+            <h2 className="text-convergence-brown leading-tight">
+              <Balancer>{images[activeImageIndex]?.title}</Balancer>
             </h2>
-            <p className="text-convergence-brown mb-6 sm:mb-8 line-clamp-3 sm:line-clamp-4 md:line-clamp-7 text-xs sm:text-sm md:text-base">
+            <p className="text-convergence-brown mb-6 line-clamp-3 sm:mb-8 sm:line-clamp-4 md:line-clamp-7">
               {images[activeImageIndex]?.caption}
             </p>
             <CMSLink
               {...images[activeImageIndex]?.link}
               appearance="ghost"
-              className="flex w-32 sm:w-48 gap-1 text-sm sm:text-base"
+              className="flex w-32 gap-1 sm:w-48"
             >
               <ChevronRight className="h-3 w-3 sm:h-4 sm:w-4" />
             </CMSLink>
--- src/components/Media/ImageMedia/index.tsx
+++ src/components/Media/ImageMedia/index.tsx
@@ -64,7 +64,7 @@ export const ImageMedia: React.FC<MediaProps> = (props) => {
     <picture>
       <NextImage
         alt={alt || ''}
-        className={cn(imgClassName)}
+        className={cn(imgClassName, 'rounded-lg')}
         fill={fill}
         height={!fill ? height : undefined}
         placeholder={placeholderFromProps ? 'blur' : undefined}
