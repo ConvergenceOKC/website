f523a6e3a3603d3dc9289464e1185a9d88d21840
James Bryan <james.bryan.home@gmail.com>
2025-09-19 21:24:41 -0500

    Add 3-column layout support to StaffBlock component

--- src/blocks/StaffBlock/Component.tsx
+++ src/blocks/StaffBlock/Component.tsx
@@ -69,6 +69,40 @@ export const StaffBlock: React.FC<StaffBlockProps> = ({ layout, staff }) => {
         ))}
       </div>
     );
+  } else if (layout === '3-col') {
+    return (
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        {staffMembers.map((member) => (
+          <div className="flex flex-col items-center gap-8" key={member.id}>
+            <Avatar className="h-44 w-44 md:h-52 md:w-52">
+              {member.headshot && (
+                <AvatarImage>
+                  <Media resource={member.headshot} />
+                </AvatarImage>
+              )}
+              <AvatarFallback className="bg-convergence-beige-darker text-convergence-teal text-4xl">
+                {member.name
+                  .split(' ')
+                  .map((n) => n[0])
+                  .join('')
+                  .toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col justify-center text-center">
+              <h5 className="mb-2">{member.name}</h5>
+              <p>
+                {member.role
+                  ?.map((role) => {
+                    if (typeof role === 'string') return role;
+                    if (typeof role === 'object') return role.name;
+                  })
+                  .join(', ')}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   } else {
     return null;
   }
