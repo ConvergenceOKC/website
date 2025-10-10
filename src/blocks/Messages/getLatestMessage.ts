2350c25eb505d4e5c2b37fbcbbc46e762c52926b
James Bryan <james.bryan.home@gmail.com>
2025-10-09 20:31:58 -0500

    refactor: add cache tag to latest message revalidation

--- src/blocks/Messages/getLatestMessage.ts
+++ src/blocks/Messages/getLatestMessage.ts
@@ -5,18 +5,22 @@ import { unstable_cache } from 'next/cache';
 import config from '@payload-config';
 import { getPayload } from 'payload';
 
-export const getLatestMessage = unstable_cache(async () => {
-  const payload = await getPayload({ config });
+export const getLatestMessage = unstable_cache(
+  async () => {
+    const payload = await getPayload({ config });
 
-  try {
-    const message = await payload.find({
-      collection: 'messages',
-      sort: '-date',
-      limit: 1,
-      depth: 2,
-    });
-    return message;
-  } catch (error) {
-    throw new Error(`Error getting latest message: ${error}`);
-  }
-}, ['latest-message']);
+    try {
+      const message = await payload.find({
+        collection: 'messages',
+        sort: '-date',
+        limit: 1,
+        depth: 2,
+      });
+      return message;
+    } catch (error) {
+      throw new Error(`Error getting latest message: ${error}`);
+    }
+  },
+  ['latest-message'],
+  { tags: ['latest-message'] },
+);
