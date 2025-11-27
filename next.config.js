8f2c8811bab8bbdd3ae5c3f7123613cb38e1ada6
James Bryan <james.bryan.home@gmail.com>
2025-11-27 08:08:37 -0600

    fix: image remote patterns for local and staging

--- next.config.js
+++ next.config.js
@@ -4,26 +4,22 @@ import redirects from './redirects.js';
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowLocalIP:
+      process.env.NODE_ENV === 'development' ? true : false,
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      // Current domain (production, staging, or localhost)
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item);
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
         };
       }),
-      {
-        protocol: 'https',
-        hostname: '**.convergenceokc.church',
-        port: '',
-        search: '',
-      },
     ],
     qualities: [100],
     localPatterns: [
