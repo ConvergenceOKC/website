e55a234a8a33dc2a04cc058cd9eb4b1ec136ef0c
James Bryan <james.bryan.home@gmail.com>
2025-11-27 10:49:03 -0600

    fix: image remote patterns for local and staging

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -1,31 +1,36 @@
-import canUseDOM from './canUseDOM'
+import canUseDOM from './canUseDOM';
 
 export const getServerSideURL = () => {
-  let url = process.env.NEXT_PUBLIC_SERVER_URL
+  let url = process.env.NEXT_PUBLIC_SERVER_URL;
 
   if (!url && process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
   if (!url) {
-    url = 'http://localhost:3000'
+    url = 'http://localhost:3000';
   }
 
-  return url
-}
+  return url;
+};
 
 export const getClientSideURL = () => {
   if (canUseDOM) {
-    const protocol = window.location.protocol
-    const domain = window.location.hostname
-    const port = window.location.port
+    const protocol = window.location.protocol;
+    const domain = window.location.hostname;
+    const port = window.location.port;
 
-    return `${protocol}//${domain}${port ? `:${port}` : ''}`
+    // If on staging and sharing database with production, use production URLs for images
+    if (domain === 'staging.convergenceokc.church') {
+      return 'https://convergenceokc.church';
+    }
+
+    return `${protocol}//${domain}${port ? `:${port}` : ''}`;
   }
 
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
 
-  return process.env.NEXT_PUBLIC_SERVER_URL || ''
-}
+  return process.env.NEXT_PUBLIC_SERVER_URL || '';
+};
