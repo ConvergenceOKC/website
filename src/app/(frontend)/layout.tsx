6ec5add973345111c24c8e35a16e68231f4cba46
James Bryan <james.bryan.home@gmail.com>
2025-12-06 09:03:33 -0600

    feat: add vercel speed insights

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,6 +4,7 @@ import localFont from 'next/font/local';
 import React from 'react';
 
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Footer } from '@/Footer/Component';
 import { Header } from '@/Header/Component';
@@ -59,6 +60,7 @@ export default async function RootLayout({
           <Footer />
           <TailwindIndicator />
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
