54891321bf20a8b2c7a9a689b646b0a874f63dee
James Bryan <james.bryan.home@gmail.com>
2026-01-15 14:05:30 -0600

    fix: cache revalidation for messages and series

--- src/collections/Messages/hooks/revalidateMessages.ts
+++ src/collections/Messages/hooks/revalidateMessages.ts
@@ -7,26 +7,38 @@ import type {
 
 import { Message } from '@/payload-types';
 
-export const revalidateMessages: CollectionAfterChangeHook<Message> = ({
+export const revalidateMessages: CollectionAfterChangeHook<Message> = async ({
   doc,
   req: { payload, context },
 }) => {
   if (!context.disableRevalidate) {
-    payload.logger.info(`Revalidating messages - message/${doc.slug}`);
+    payload.logger.info(
+      `Revalidating messages - message/${doc.slug} & series/${doc.series}`,
+    );
+    const series = await payload.findByID({
+      collection: 'messageSeries',
+      id: doc.series as string,
+    });
     revalidateTag('messages', 'max');
     revalidateTag(doc.slug || '', 'max'); // This is for the individual message page
+    revalidateTag(series.slug || '', 'max'); // This is for the series page
   }
 
   return doc;
 };
 
-export const revalidateDelete: CollectionAfterDeleteHook<Message> = ({
+export const revalidateDelete: CollectionAfterDeleteHook<Message> = async ({
   doc,
-  req: { context },
+  req: { payload, context },
 }) => {
   if (!context.disableRevalidate) {
+    const series = await payload.findByID({
+      collection: 'messageSeries',
+      id: doc.series as string,
+    });
     revalidateTag('messages', 'max');
     revalidateTag(doc.slug || '', 'max'); // This is for the individual message page
+    revalidateTag(series.slug || '', 'max'); // This is for the series page
   }
 
   return doc;
